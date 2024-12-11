4ec1fb82e600e11b04789cf5b2dea1f4df4e3f65
ebagabee <gabrielhas.tech@gmail.com>
2024-12-11 16:14:11 -0300

    feat: adiciona botao de criar lista

--- src/app/lists.tsx
+++ src/app/lists.tsx
@@ -1,10 +1,15 @@
 import { View, Image } from "react-native";
 import Header from "../components/header";
+import AddButton from "../components/add-button";
 
 export default function Lists() {
   return (
     <View>
       <Header />
+
+      <View className="mt-10">
+        <AddButton />
+      </View>
     </View>
   );
 }
