d25fcd39231c908664eb337a2998b7f47f70f574
ebagabee <gabrielhas.tech@gmail.com>
2024-12-12 21:21:08 -0300

    feat: render the list after redirect to new page list

--- src/app/lists.tsx
+++ src/app/lists.tsx
@@ -1,23 +1,64 @@
 import Header from "../components/header";
+
 import { Feather } from "@expo/vector-icons";
 import { router } from "expo-router";
-
-import { TouchableOpacity, View } from "react-native";
-
-const redirectToNewList = () => {
-  router.push("/newList");
-};
+import { FlatList, TouchableOpacity, View, Text } from "react-native";
+import { ListDatabase, useListDatabase } from "../database/useListDatabase";
+import { useEffect, useState } from "react";
 
 export default function Lists() {
+  const { create, findAll } = useListDatabase();
+  const [lists, setLists] = useState<ListDatabase[]>([]);
+
+  const listDatabase = useListDatabase();
+
+  const redirectToNewList = async () => {
+    try {
+      const { insertedRowId } = await create({ name: "Nova Lista" });
+
+      router.push({
+        pathname: "/newList",
+        params: { listId: insertedRowId },
+      });
+    } catch (error) {
+      console.error("Erro ao criar uma nova lista", error);
+    }
+  };
+
+  const getAllLists = async () => {
+    try {
+      const response = await listDatabase.findAll();
+      if (response) {
+        setLists(response as ListDatabase[]);
+      }
+    } catch (error) {
+      console.error("Erro ao buscar listas", error);
+    }
+  };
+
+  useEffect(() => {
+    getAllLists();
+  }, []);
+
   return (
     <View className="flex-col bg-white">
       <Header default editable={false} />
 
-      {/* TODO: Flatlists */}
+      <FlatList
+        data={lists}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View className="px-10 mt-4">
+            <TouchableOpacity className="p-6 bg-orange-500 mt-2 rounded-2xl">
+              <Text className="text-white font-bold">{item.name}</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
 
       <View className="px-10">
         <TouchableOpacity
-          className="p-6 bg-orange-500 mt-10"
+          className="p-6 bg-orange-500 mt-10 rounded-2xl"
           onPress={redirectToNewList}
         >
           <Feather name="plus" size={20} className="text-center" color="#FFF" />
--- src/app/newList.tsx
+++ src/app/newList.tsx
@@ -1,12 +1,37 @@
 import { View, Text } from "react-native";
 import Header from "../components/header";
 import FloatButton from "../components/float-button";
+import { useState } from "react";
+import { useLocalSearchParams } from "expo-router";
+import { useListDatabase } from "../database/useListDatabase";
 
 export default function NewListScreen() {
+  const { listId } = useLocalSearchParams();
+  const [listName, setListName] = useState("Nova Lista");
+
+  // Datas about the product
+  const [id, setId] = useState();
+  const [nameItem, setNameItem] = useState();
+  const [priceItem, setPriceItem] = useState();
+  const [quantityItem, setQuantityItem] = useState();
+  const [inCart, setInCart] = useState(false);
+
+  // Actions
+  const [showOptions, setShowOptions] = useState();
+
+  const updateListName = async (newName: string) => {
+    // await update(listId, {name: newName})
+    setListName(newName);
+  };
+
   return (
     <View className="h-full w-full">
-      <Header title="Minha Lista" editable={true} default={false} />
-
+      <Header
+        title="Minha Lista"
+        editable
+        default={false}
+        onEditTitle={updateListName}
+      />
       <FloatButton />
     </View>
   );
