43f90bca7b88e2cb72ad37ed6cda5b10066b65a9
ebagabee <ebagabe.2025@gmail.com>
2025-05-17 11:34:35 -0300

    feat: change the StatusBar color to black and the content to white

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,11 +3,18 @@ import { Slot } from "expo-router";
 import "../global.css";
 import { SQLiteProvider } from "expo-sqlite";
 import { initializeDatabase } from "../database/initializeDatabase";
+import { StatusBar, View } from "react-native";
 
 export default function Layout() {
   return (
     <SQLiteProvider databaseName="database.db" onInit={initializeDatabase}>
-      <Slot />
+      <View>
+        <StatusBar 
+          backgroundColor="#000000"
+          barStyle="light-content"
+        />
+        <Slot />
+      </View>
     </SQLiteProvider>
   );
 }
